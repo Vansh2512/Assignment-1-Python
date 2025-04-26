3b0a1fd2a47076720252ccfeba6e65652fd6a19b
VANSH RATHOD <113336212+Vansh2512@users.noreply.github.com>
2025-04-26 16:33:42 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,12 @@
+a=int(input("Input of A:"))
+b=int(input("Input of B:"))
+
+add=a+b
+sub=a-b
+mul=a*b
+div=a/b
+
+print("Addition of A and B is: ",add)
+print("Subtraction of A and B is: ",sub)
+print("Multiplication of A and B:",mul)
+print("Division of A and B is: ",div)
---
+++ Task2.py
@@ -0,0 +1,5 @@
+a=input("Enter Your First Name:")
+b=input("Enter Your Last Name:")
+
+print("Hello, "+a+" "+b+"! Welcome to Python Program.")
+
